c843fc76246df249a5d3ce171383147881b4d127
Nijat Zeynalov <31247506+NijatZeynalov@users.noreply.github.com>
2021-01-15 15:17:25 +0400

    Update train.py

--- tacotron/train.py
+++ tacotron/train.py
@@ -36,10 +36,10 @@ def add_stats(model):
 def time_string():
 	return datetime.now().strftime('%Y-%m-%d %H:%M')
 
-def train(log_dir, args):
+def train(log_dir):
 	save_dir = os.path.join(log_dir, 'pretrained/')
 	checkpoint_path = os.path.join(save_dir, 'model.ckpt')
-	input_path = os.path.join(args.base_dir, args.input)
+	input_path = os.path.join(base_dir, input)
 	plot_dir = os.path.join(log_dir, 'plots')
 	wav_dir = os.path.join(log_dir, 'wavs')
 	mel_dir = os.path.join(log_dir, 'mel-spectrograms')
@@ -53,7 +53,7 @@ def train(log_dir, args):
 
 	log('Checkpoint path: {}'.format(checkpoint_path))
 	log('Loading training data from: {}'.format(input_path))
-	log('Using model: {}'.format(args.model))
+	log('Using model: {}'.format(model))
 	log(hparams_debug_string())
 
 	#Set up data feeder
@@ -72,7 +72,7 @@ def train(log_dir, args):
 
 	global_step = tf.Variable(step_count, name='global_step', trainable=False)
 	with tf.variable_scope('model') as scope:
-		model = create_model(args.model, hparams)
+		model = create_model(model, hparams)
 		if hparams.predict_linear:
 			model.initialize(feeder.inputs, feeder.input_lengths, feeder.mel_targets, feeder.token_targets, feeder.linear_targets)
 		else:
@@ -98,7 +98,7 @@ def train(log_dir, args):
 			sess.run(tf.global_variables_initializer())
 
 			#saved model restoring
-			if args.restore:
+			if restore:
 				#Restore saved model if the user requested it, Default = True.
 				try:
 					checkpoint_state = tf.train.get_checkpoint_state(save_dir)
@@ -110,7 +110,7 @@ def train(log_dir, args):
 				saver.restore(sess, checkpoint_state.model_checkpoint_path)
 
 			else:
-				if not args.restore:
+				if not restore:
 					log('Starting new training!')
 				else:
 					log('No model to load at {}'.format(save_dir))
@@ -132,11 +132,11 @@ def train(log_dir, args):
 					log('Loss exploded to {:.5f} at step {}'.format(loss, step))
 					raise Exception('Loss exploded')
 
-				if step % args.summary_interval == 0:
+				if step % summary_interval == 0:
 					log('\nWriting summary at step: {}'.format(step))
 					summary_writer.add_summary(sess.run(stats), step)
 				
-				if step % args.checkpoint_interval == 0:
+				if step % checkpoint_interval == 0:
 					with open(os.path.join(log_dir,'step_counter.txt'), 'w') as file:
 						file.write(str(step))
 					log('Saving checkpoint to: {}-{}'.format(checkpoint_path, step))
@@ -177,13 +177,13 @@ def train(log_dir, args):
 
 					#save alignment plot to disk (control purposes)
 					plot.plot_alignment(alignment, os.path.join(plot_dir, 'step-{}-align.png'.format(step)),
-						info='{}, {}, step={}, loss={:.5f}'.format(args.model, time_string(), step, loss))
+						info='{}, {}, step={}, loss={:.5f}'.format(model, time_string(), step, loss))
 					#save real mel-spectrogram plot to disk (control purposes)
 					plot.plot_spectrogram(target, os.path.join(plot_dir, 'step-{}-real-mel-spectrogram.png'.format(step)),
-						info='{}, {}, step={}, Real'.format(args.model, time_string(), step, loss))
+						info='{}, {}, step={}, Real'.format(model, time_string(), step, loss))
 					#save predicted mel-spectrogram plot to disk (control purposes)
 					plot.plot_spectrogram(mel_prediction, os.path.join(plot_dir, 'step-{}-pred-mel-spectrogram.png'.format(step)),
-						info='{}, {}, step={}, loss={:.5}'.format(args.model, time_string(), step, loss))
+						info='{}, {}, step={}, loss={:.5}'.format(model, time_string(), step, loss))
 					log('Input at step {}: {}'.format(step, sequence_to_text(input_seq)))
 
 		except Exception as e:
@@ -191,11 +191,11 @@ def train(log_dir, args):
 			traceback.print_exc()
 			coord.request_stop(e)
 
-def tacotron_train(args):
-	hparams.parse(args.hparams)
-	os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(args.tf_log_level)
-	run_name = args.name or args.model
-	log_dir = os.path.join(args.base_dir, 'logs-{}'.format(run_name))
+def tacotron_train():
+#	hparams.parse(args.hparams)
+	os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(1)
+	run_name = name or model
+	log_dir = os.path.join(base_dir, 'logs-{}'.format(run_name))
 	os.makedirs(log_dir, exist_ok=True)
 	infolog.init(os.path.join(log_dir, 'Terminal_train_log'), run_name)
-	train(log_dir, args)
+	train(log_dir)
